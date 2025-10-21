09ecc85194bfc0ef7714f66847dd9da245ccef89
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-21 14:20:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ genero3 = ["","norte","Sul","Leste","Oeste"]
 
 select1 = st.selectbox("Escolha uma Direção:",genero1)
 select2 = st.selectbox("Escolha uma Direção:",genero2)
-select3 = st.selectbox("Escolha uma Direção:",genero3)
+
 st.button("Pesquisar")
 st.write(
     f"Voce escolheu a direção:{select1}"
