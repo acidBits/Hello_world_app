7c17b529fc2b893b7d3827b60e0e78e8827c8459
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-21 14:06:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ st.title("Meu Aplicativo")
 st.write(
     "Testando meu aplicativo"
 )
+
+opcoes = ["norte","Sul","Leste","Oeste"]
+select = st.selectbox("Escolha uma Direção:",opcoes)
+st.write(
+    f"Voce selecionou:{select}"
+)
