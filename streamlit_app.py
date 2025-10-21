4b9b035add81552576803facc6de641f3700a1e9
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-21 14:26:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ genero1 = ["","norte","Sul","Leste","Oeste"]
 genero2 = ["","norte","Sul","Leste","Oeste"]
 genero3 = ["","norte","Sul","Leste","Oeste"]
 
-genero1 = st.selectbox("Escolha uma Direção:",genero1)
-genero2 = st.selectbox("escolha",["", "1","2"])
-genero3 = st.selectbox("",genero3)
+genero1 = st.selectbox("Genero-1:",genero1)
+genero2 = st.selectbox("Genero-2:",genero2)
+genero3 = st.selectbox("Genero-3:",genero3)
 
 st.button("Pesquisar")
 st.write(
