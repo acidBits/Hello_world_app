4475846920edb79fb4deee7d58b0acf3e6a11788
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-21 13:09:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Meu Aplicativo")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Testando meu aplicativo"
 )
