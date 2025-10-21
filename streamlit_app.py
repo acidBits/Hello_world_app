74d41e35b733c77570b60fa66b4636c0d475b8cf
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-21 14:49:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
 import streamlit as st
+import pandas as pd
+import joblib
 
 st.title("Me Indique um Filme 🎬")
 st.divider()
 
-genero1 = ["","norte","Sul","Leste","Oeste"]
-genero2 = ["","norte","Sul","Leste","Oeste"]
-genero3 = ["","norte","Sul","Leste","Oeste"]
+genero = ["","Terror","Comedy","Romance","Action"]
 
-genero1 = st.selectbox("Genero-1:",genero1)
-genero2 = st.selectbox("Genero-2:",genero2)
-genero3 = st.selectbox("Genero-3:",genero3)
+genero1 = st.selectbox("Genero-1:",genero)
+genero2 = st.selectbox("Genero-2:",genero)
+genero3 = st.selectbox("Genero-3:",genero)
 
 st.markdown("<br><br>", unsafe_allow_html=True)
 if st.button("Pesquisar"):
