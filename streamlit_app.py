76d1345bc27b4e1589e6d9f2808f57bf7660475f
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-21 14:28:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ genero1 = st.selectbox("Genero-1:",genero1)
 genero2 = st.selectbox("Genero-2:",genero2)
 genero3 = st.selectbox("Genero-3:",genero3)
 
-st.button("Pesquisar")
-st.write(
-    f"Voce escolheu a direção:{genero1}"
-)
+if st.button("Pesquisar"):
+    st.write(f"A direção Indicada é: {genero1}")
+
