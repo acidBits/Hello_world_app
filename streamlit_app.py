29a2ff7f7a2946b844b7d5c4f95cf82eb61ebb8c
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-21 15:31:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,21 @@ from sklearn.metrics.pairwise import cosine_similarity
 url = "https://raw.githubusercontent.com/acidBits/Hello_world_app/refs/heads/main/movies.csv"
 df = pd.read_csv(url)
 
+# Inicializando o vetorizador
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(df['generos'])
+generos_agrupados = df.explode('generos')['generos'].unique()
+generos_unicos = sorted(set(g for lista in generos_agrupados for g in lista.split(', ')))
+
+
 st.title("Me Indique um Filme 🎬")
 st.divider()
 
 genero = ["","Terror","Comedy","Romance","Action"]
 
-genero1 = st.selectbox("Genero-1:",genero)
-genero2 = st.selectbox("Genero-2:",genero)
-genero3 = st.selectbox("Genero-3:",genero)
+genero1 = st.selectbox("Genero-1:",generos_unicos)
+genero2 = st.selectbox("Genero-2:",generos_unicos)
+genero3 = st.selectbox("Genero-3:",generos_unicos)
 
 st.markdown("<br><br>", unsafe_allow_html=True)
 if st.button("Pesquisar"):
