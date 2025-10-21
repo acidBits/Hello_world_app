fe10615781bed7775958b280cac706c161a8b345
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-21 14:17:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,13 @@ import streamlit as st
 st.title("Me Indique um Filme")
 
 genero1 = ["","norte","Sul","Leste","Oeste"]
-select = st.selectbox("Escolha uma Direção:",genero1)
-select = st.selectbox("Escolha uma Direção:",genero1)
-select = st.selectbox("Escolha uma Direção:",genero1)
+genero2 = ["","norte","Sul","Leste","Oeste"]
+genero3 = ["","norte","Sul","Leste","Oeste"]
+
+select1 = st.selectbox("Escolha uma Direção:",genero1)
+select2 = st.selectbox("Escolha uma Direção:",genero2)
+select3 = st.selectbox("Escolha uma Direção:",genero3)
 st.button("Pesquisar")
 st.write(
-    f"Voce escolheu a direção:{select}"
+    f"Voce escolheu a direção:{select1}"
 )
